0e2e8561a27ba79c9d3503da59e82362d296f367
Matilde Aragao <matilde.aragao@grupofullsix.pt>
2020-04-16 16:23:26 +0100

    phonebook step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,30 +5,33 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const handleSubmit =(e)=>{
     e.preventDefault();
 
     const duplicateName = persons.filter(person => person.name === newName)
-
     if(duplicateName.length){
-
       alert(`${newName} is already added to phonebook`) 
       return
     } 
     
     
-
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     
     setPersons(persons.concat(newPerson))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -39,11 +42,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit" onClick={handleSubmit}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, i) => <li key={i}>{person.name}</li>)}
+      {persons.map((person, i) => <li key={i}>{person.name} {person.number}</li>)}
     </div>
   )
 }
