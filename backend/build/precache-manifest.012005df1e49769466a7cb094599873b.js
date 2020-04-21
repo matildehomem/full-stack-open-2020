72734afb7de3ed23dccfff96a169c30d10bad281
Matilde Aragao <matilde.aragao@grupofullsix.pt>
2020-04-21 18:24:07 +0100

    changes in backend notes

---
+++ backend/build/precache-manifest.012005df1e49769466a7cb094599873b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0106ae3374a414fa43a02eb7a3ea20fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fad4599c72bda76190d6",
+    "url": "/static/css/main.73334574.chunk.css"
+  },
+  {
+    "revision": "42d7708cf424b0162320",
+    "url": "/static/js/2.b3a5bfc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b3a5bfc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad4599c72bda76190d6",
+    "url": "/static/js/main.f7360942.chunk.js"
+  },
+  {
+    "revision": "2cfd5c9ca41a186dc3e7",
+    "url": "/static/js/runtime-main.b93edf6a.js"
+  }
+]);
--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
+app.use(express.static('build'))
+
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
